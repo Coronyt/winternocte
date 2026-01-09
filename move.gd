97db96333dfc4c04942980d20277ffac5095135a
coro <101323591+Coronyt@users.noreply.github.com>
2026-01-08 19:32:38 -0600

    Collision check moved to separate function

--- move.gd
+++ move.gd
@@ -6,36 +6,35 @@ func _init(_actor: Entity, _dir: Game.Direction):
 	self.actor = _actor
 	self.dir = _dir
 
+# will return false if the movement would result in a collision
+func check_collision(dir: Game.Direction) -> bool:
+	var cand_x = self.actor.pos.x
+	var cand_y = self.actor.pos.y
+	if self.dir == Game.Direction.UP:
+		cand_y -= 1
+	if self.dir == Game.Direction.DOWN:
+		cand_y += 1
+	if self.dir == Game.Direction.LEFT:
+		cand_x -= 1
+	if self.dir == Game.Direction.RIGHT:
+		cand_x += 1
+	var cand_pos = Position.new(cand_x, cand_y)
+	var cand_cell = Game.active_canvas.get_cell(cand_pos)
+	if Atlas.cells[cand_cell.id].can_collide:
+		return false
+	return true
+
 # move the actor's position coords to the target destination
 func perform():
 	if self.dir == Game.Direction.UP:
-		#print("up command is being performed")
-		# TODO - move collision check to func to remove redundant code
-		var cand_pos = Position.new(self.actor.pos.x, self.actor.pos.y - 1)
-		var cand_cell = Game.active_canvas.get_cell(cand_pos)
-		#print(Atlas.cells[cand_cell.id].glyph)
-		#print(Atlas.cells[cand_cell.id].can_collide)
-		if Atlas.cells[cand_cell.id].can_collide:
-			return
-		self.actor.pos.y = self.actor.pos.y - 1
+		if check_collision(Game.Direction.UP):
+			self.actor.pos.y = self.actor.pos.y - 1
 	if self.dir == Game.Direction.DOWN:
-		#print("down command is being performed")
-		var cand_pos = Position.new(self.actor.pos.x, self.actor.pos.y + 1)
-		var cand_cell = Game.active_canvas.get_cell(cand_pos)
-		if Atlas.cells[cand_cell.id].can_collide:
-			return
-		self.actor.pos.y = self.actor.pos.y + 1
+		if check_collision(Game.Direction.DOWN):
+			self.actor.pos.y = self.actor.pos.y + 1
 	if self.dir == Game.Direction.LEFT:
-		#print("left command is being performed")
-		var cand_pos = Position.new(self.actor.pos.x - 1, self.actor.pos.y)
-		var cand_cell = Game.active_canvas.get_cell(cand_pos)
-		if Atlas.cells[cand_cell.id].can_collide:
-			return
-		self.actor.pos.x = self.actor.pos.x - 1
+		if check_collision(Game.Direction.LEFT):
+			self.actor.pos.x = self.actor.pos.x - 1
 	if self.dir == Game.Direction.RIGHT:
-		#print("right command is being performed")
-		var cand_pos = Position.new(self.actor.pos.x + 1, self.actor.pos.y)
-		var cand_cell = Game.active_canvas.get_cell(cand_pos)
-		if Atlas.cells[cand_cell.id].can_collide:
-			return
-		self.actor.pos.x = self.actor.pos.x + 1
+		if check_collision(Game.Direction.RIGHT):
+			self.actor.pos.x = self.actor.pos.x + 1
