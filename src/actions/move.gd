703d7909002c1bfa8c5ca8e6a48c4bcbbb957a32
coro <101323591+Coronyt@users.noreply.github.com>
2026-01-08 20:54:35 -0600

    Reorganized game file directories

--- src/actions/move.gd
+++ src/actions/move.gd
@@ -7,7 +7,7 @@ func _init(_actor: Entity, _dir: Game.Direction):
 	self.dir = _dir
 
 # will return false if the movement would result in a collision
-func check_collision(dir: Game.Direction) -> bool:
+func check_collision() -> bool:
 	var cand_x = self.actor.pos.x
 	var cand_y = self.actor.pos.y
 	if self.dir == Game.Direction.UP:
@@ -20,7 +20,7 @@ func check_collision(dir: Game.Direction) -> bool:
 		cand_x += 1
 	var cand_pos = Position.new(cand_x, cand_y)
 	var cand_cell = Game.active_canvas.get_cell(cand_pos)
-	print(cand_cell.entities)
+	#print(cand_cell.entities)
 	# checking for entities in the target cell
 	for entity in cand_cell.entities:
 		if entity is Door:
@@ -31,15 +31,11 @@ func check_collision(dir: Game.Direction) -> bool:
 
 # move the actor's position coords to the target destination
 func perform():
-	if self.dir == Game.Direction.UP:
-		if check_collision(Game.Direction.UP):
-			self.actor.pos.y = self.actor.pos.y - 1
-	if self.dir == Game.Direction.DOWN:
-		if check_collision(Game.Direction.DOWN):
-			self.actor.pos.y = self.actor.pos.y + 1
-	if self.dir == Game.Direction.LEFT:
-		if check_collision(Game.Direction.LEFT):
-			self.actor.pos.x = self.actor.pos.x - 1
-	if self.dir == Game.Direction.RIGHT:
-		if check_collision(Game.Direction.RIGHT):
-			self.actor.pos.x = self.actor.pos.x + 1
+	if self.dir == Game.Direction.UP and check_collision():
+		self.actor.pos.y = self.actor.pos.y - 1
+	if self.dir == Game.Direction.DOWN and check_collision():
+		self.actor.pos.y = self.actor.pos.y + 1
+	if self.dir == Game.Direction.LEFT and check_collision():
+		self.actor.pos.x = self.actor.pos.x - 1
+	if self.dir == Game.Direction.RIGHT and check_collision():
+		self.actor.pos.x = self.actor.pos.x + 1
