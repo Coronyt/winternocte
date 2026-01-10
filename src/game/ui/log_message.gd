65ed63b83746c6644757b42b966b894b5d1775c3
coro <101323591+Coronyt@users.noreply.github.com>
2026-01-10 12:01:47 -0600

    Log message fade animation

--- src/game/ui/log_message.gd
+++ src/game/ui/log_message.gd
@@ -1,1 +1,9 @@
 extends Node
+
+@onready var anim = $_log_message_anim
+
+func _on_log_message_timer_timeout():
+	anim.play("fade")
+
+func _on_log_message_animation_finished(_anim_name):
+	self.queue_free()
