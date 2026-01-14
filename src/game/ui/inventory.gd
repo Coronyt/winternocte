8ed231046ef230f7a5573a1e7e91e5732705d4c2
coro <101323591+Coronyt@users.noreply.github.com>
2026-01-13 19:42:29 -0600

    Settled on inventory UI concept

--- src/game/ui/inventory.gd
+++ src/game/ui/inventory.gd
@@ -1,4 +1,21 @@
 extends Control
 
-@onready var _inventory_list = $_inventory_bg/_inventory_list
-@onready var _inventory_anims = $_inventory_anims
+# TODO - idea for the inventory UI: the buttons should only appear when the user mouses over the entry
+# also the glyph associated with the item should be displayed here too
+# maybe at the end of the entry if the buttons aren't active
+
+@onready var list = $_inventory_bg/_inventory_list
+@onready var anims = $_inventory_anims
+
+# TODO - this var should probably exist in a global script
+# since other functions which are waiting for input may need to know if the inventory is open
+var is_open: bool = false
+
+func _process(_delta):
+	if Input.is_action_just_pressed("inventory"):
+		if self.is_open:
+			anims.play("close_inv")
+			self.is_open = false
+		else:
+			anims.play("open_inv")
+			self.is_open = true
--- src/game/ui/log.gd
+++ src/game/ui/log.gd
@@ -2,10 +2,10 @@ class_name Log extends Node
 
 const LogMessage = preload("res://game/ui/log_message.tscn")
 
-@onready var _log_window = $_log_window
+@onready var log_window = $_log_window
 
 # create a new log_message instance and add it as a child of log_window
 func log_event(message: String):
 	var new_log_message = LogMessage.instantiate()
 	new_log_message.set_message_body(message)
-	_log_window.add_child(new_log_message)
+	log_window.add_child(new_log_message)
