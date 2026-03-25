65b65f9cfab4b9a4b4036f6c61ef1be3cc86b608
coro <101323591+Coronyt@users.noreply.github.com>
2026-03-24 19:53:32 -0500

    Moved candidate position logic from check_collision() to _init()

--- src/actions/move.gd
+++ src/actions/move.gd
@@ -2,14 +2,17 @@ class_name Move extends Action
 
 var dir: Game.Direction
 
+var cand_x: int
+var cand_y: int
+#var cand_x = self.actor.pos.x
+#var cand_y = self.actor.pos.y
+
 func _init(_actor: Entity, _dir: Game.Direction):
 	self.actor = _actor
 	self.dir = _dir
-
-# will return false if the movement would result in a collision
-func check_collision() -> bool:
-	var cand_x = self.actor.pos.x
-	var cand_y = self.actor.pos.y
+	# get candidate coords from direction
+	self.cand_x = _actor.pos.x
+	self.cand_y = _actor.pos.y
 	if self.dir == Game.Direction.UP:
 		cand_y -= 1
 	if self.dir == Game.Direction.DOWN:
@@ -18,6 +21,19 @@ func check_collision() -> bool:
 		cand_x -= 1
 	if self.dir == Game.Direction.RIGHT:
 		cand_x += 1
+
+# will return false if the movement would result in a collision
+func check_collision() -> bool:
+	#var cand_x = self.actor.pos.x
+	#var cand_y = self.actor.pos.y
+	#if self.dir == Game.Direction.UP:
+		#cand_y -= 1
+	#if self.dir == Game.Direction.DOWN:
+		#cand_y += 1
+	#if self.dir == Game.Direction.LEFT:
+		#cand_x -= 1
+	#if self.dir == Game.Direction.RIGHT:
+		#cand_x += 1
 	var cand_pos = Position.new(cand_x, cand_y)
 	var cand_cell = Game.active_canvas.get_cell(cand_pos)
 	#print(cand_cell.entities)
@@ -29,8 +45,16 @@ func check_collision() -> bool:
 		return false
 	return true
 
+# TODO
+func check_bounds():
+	pass
+
 # move the actor's position coords to the target destination
 func perform():
+	# ___
+	# TODO - need to check if target position is valid first
+	# TODO - then after that check is implemented, we can implement level-changing on the world map
+	# ___
 	if self.dir == Game.Direction.UP and check_collision():
 		self.actor.pos.y = self.actor.pos.y - 1
 	if self.dir == Game.Direction.DOWN and check_collision():
@@ -39,3 +63,6 @@ func perform():
 		self.actor.pos.x = self.actor.pos.x - 1
 	if self.dir == Game.Direction.RIGHT and check_collision():
 		self.actor.pos.x = self.actor.pos.x + 1
+	# ___
+	print("position after moving: ", self.actor.pos.x, ", ", self.actor.pos.y)
+	# ___
