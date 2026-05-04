cfea60f2242c913dd97d714edcbc5e9fc70bafb0
coro <101323591+Coronyt@users.noreply.github.com>
2026-05-04 18:01:25 -0500

    WorldCell loads level data from /levels directory according to Position

--- src/world/world_cell.gd
+++ src/world/world_cell.gd
@@ -1,16 +1,20 @@
 class_name WorldCell extends Node
 
 var pos: Position
-var level: Array[Array]
+var level: Array
 var entities: EntityGroup
 
-# TODO: constructor func could take Position as arg
-# and from there it would go to the appropriate directory e.g. "levels/XX"
-# and from there load the level matrix and the entity group into this worldcell
-	# this worldcell should then be accessible via the Game World Dictionary
-	# i.e. the position [0, 0] will map to this worldcell
+var data_dir: String
 
 func _init(_pos: Position):
 	self.pos = _pos
-	#print(_pos.x, _pos.y)
-	# TODO - load level matrix and entities from "levels/${_pos.x, _pos.y}"
+	# format pos for string paths
+	var pos_str = str(_pos.x) + str(_pos.y)
+	# build path to data directory for this worldcell
+	self.data_dir = "res://src/world/levels/" + pos_str
+	# load level matrix into self.level
+	var _matrix = load(data_dir + "/" + pos_str + "_matrix.gd")
+	self.level = _matrix.rows
+	# load entities into self.entities
+	var _entities = load(data_dir + "/" + pos_str + "_entities.tscn")
+	self.entities = _entities.instantiate()
