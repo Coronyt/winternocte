83d7d0217b992192b9399ec5424b26e8e05981d6
coro <101323591+Coronyt@users.noreply.github.com>
2026-05-04 16:31:38 -0500

    Created files for WorldCell and WorldDictionary structures

---
+++ src/world/world_cell.gd
@@ -0,0 +1,4 @@
+class_name WorldCell extends Node
+
+var level: Array[Array]
+var entities: EntityGroup
---
+++ src/world/world_dictionary.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var GameWorld: Dictionary[Position, WorldCell] = {
+	# TODO
+	Position.new(0, 0) : WorldCell.new()
+}
